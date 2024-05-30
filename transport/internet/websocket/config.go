33261ad9f82b4a4ff689e301df2564ee6db06a7a
aiempire79 <130184078+aiempire79@users.noreply.github.com>
2024-05-30 12:03:50 +0330

    Update config.go

--- transport/internet/websocket/config.go
+++ transport/internet/websocket/config.go
@@ -2,6 +2,9 @@ package websocket
 
 import (
 	"net/http"
+	"time"
+	"unicode"
+	"math/rand"
 
 	"github.com/xtls/xray-core/common"
 	"github.com/xtls/xray-core/transport/internet"
@@ -25,8 +28,23 @@ func (c *Config) GetRequestHeader() http.Header {
 	for k, v := range c.Header {
 		header.Add(k, v)
 	}
-	header.Set("Host", c.Host)
+	randomizedHost := randomizeCase(c.Host)
+	header.Set("Host", randomizedHost)
+	header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 11; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.77 Mobile Safari/537.36")
 	return header
+}
+
+func randomizeCase(s string) string {
+	rand.Seed(time.Now().UnixNano())
+	runes := []rune(s)
+	for i, r := range runes {
+		if rand.Intn(2) == 0 {
+			runes[i] = unicode.ToLower(r)
+		} else {
+			runes[i] = unicode.ToUpper(r)
+		}
+	}
+	return string(runes)
 }
 
 func init() {
